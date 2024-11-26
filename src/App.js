18de786be7c3f0369730858ca34a1b7340604f53
spoyodevelop <kjy9505@gmail.com>
2024-11-26 17:07:08 +0900

    feat: 달력 구현 성공

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ class App {
   async run() {
     const { monthAndWeekDay, weekdayList, weekendList } =
       await InputView.getAllInput();
-    const hoildays = [
-      { month: 1, day: 1 },
-      { month: 3, day: 1 },
-      { month: 5, day: 5 },
-      { month: 6, day: 6 },
-      { month: 8, day: 15 },
-      { month: 10, day: 3 },
-      { month: 10, day: 9 },
-      { month: 12, day: 15 },
+    const holidays = [
+      { holidayMonth: 1, holiday: 1 },
+      { holidayMonth: 3, holiday: 1 },
+      { holidayMonth: 5, holiday: 5 },
+      { holidayMonth: 6, holiday: 6 },
+      { holidayMonth: 8, holiday: 15 },
+      { holidayMonth: 10, holiday: 3 },
+      { holidayMonth: 10, holiday: 9 },
+      { holidayMonth: 12, holiday: 25 },
     ];
 
     // 일자를 집어 넣으면 그때부터 시작해서 계속해서 토요일을 돌려 받는 함수를 하나 만들어라.
@@ -24,22 +24,22 @@ class App {
       const monthDates = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
       const monthDateforSelectedMonth = monthDates[month - 1];
       const datesList = [];
-      for (let i = 1; i < monthDateforSelectedMonth + 1; i++) {
-        // hoildays.some((month, day) => {
-        //   if (month === month && day === i) return true;
-        // });
+      for (let day = 1; day < monthDateforSelectedMonth + 1; day++) {
+        const isHoliday = holidays.some(
+          (holiday) =>
+            holiday.holidayMonth === month && holiday.holiday === day,
+        );
 
         datesList.push({
           month,
-          date: i,
-          day: days[(days.indexOf(weekday) + i - 1) % 7],
+          date: day,
+          day: days[(days.indexOf(weekday) + day - 1) % 7],
+          isHoliday,
         });
       }
 
       return datesList;
     }
-
-    console.log(getDateandDay(month, weekday));
   }
 }
 
