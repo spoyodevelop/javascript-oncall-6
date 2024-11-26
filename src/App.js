595ca31f16a2fda702ea1a09f5e4613f10c8a560
spoyodevelop <kjy9505@gmail.com>
2024-11-26 16:06:11 +0900

    feat:validator 구현 완료

--- src/App.js
+++ src/App.js
@@ -5,16 +5,8 @@ class App {
     // const { number, day } = await InputView.getValidDateAndDay();
     // console.log(number, day);
 
-    const nameList = await InputView.getValidShoppingCart();
-    console.log(nameList);
-
-    // const arr = ['사과', '배', '바나나', '사과', '딸기'];
-
-    // const hasDuplicates = arr.length !== new Set(arr).size;
-
-    // console.log(hasDuplicates ? '중복 있음' : '중복 없음'); // 중복 있음
-    // // console.log(isDuplicateList(nameList));
-    // // console.log(new Set(nameList));
+    const { dateAndDay, weekday, weekend } = await InputView.getAllInput();
+    console.log(dateAndDay, weekday, weekend);
   }
 }
 
--- src/InputView.js
+++ src/InputView.js
@@ -3,32 +3,58 @@ import validateNameList from './Validation/validateShoppingCart.js';
 import validateDateNumber from './Validation/validateDateNumber.js';
 
 const InputView = {
-  async getValidShoppingCart() {
-    while (true) {
-      const input = await Console.readLineAsync(
-        '평일 비상 근무 순번대로 사원 닉네임을 입력하세요>',
-      );
+  async getValidWeekdayList() {
+    const input = await Console.readLineAsync(
+      '평일 비상 근무 순번대로 사원 닉네임을 입력하세요>',
+    );
 
-      const nameList = validateNameList(input);
+    const nameList = validateNameList(input);
 
-      if (nameList !== null) {
-        return nameList;
-      }
+    if (nameList !== null) {
+      return nameList;
     }
   },
-  async getAllinput() {},
+  async getValidWeekendList() {
+    const input = await Console.readLineAsync(
+      '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>',
+    );
+
+    const nameList = validateNameList(input);
+
+    if (nameList !== null) {
+      return nameList;
+    }
+  },
   async getValidDateAndDay() {
+    const input = await Console.readLineAsync(
+      '비상 근무를 배정할 월과 시작 요일을 입력하세요>',
+    );
+
+    const result = validateDateNumber(input);
+
+    if (result) {
+      const { number, day } = result;
+      return { number, day };
+    }
+  },
+  async getAllInput() {
     while (true) {
-      const input = await Console.readLineAsync(
-        '비상 근무를 배정할 월과 시작 요일을 입력하세요>',
-      );
-
-      const result = validateDateNumber(input);
-
-      if (result) {
-        const { number, day } = result;
-        return { number, day };
+      const dateAndDay = await this.getValidDateAndDay();
+      if (!dateAndDay) {
+        continue;
       }
+
+      const weekday = await this.getValidWeekdayList();
+      if (!weekday) {
+        continue;
+      }
+      const weekend = await this.getValidWeekendList();
+      if (!weekend) {
+        continue;
+      }
+
+      if (dateAndDay && weekday && weekend)
+        return { dateAndDay, weekday, weekend };
     }
   },
 };
